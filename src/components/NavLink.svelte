5f659b910741c0a2e952a8aa1bcfbfc7f0bcc423
andrewhwaller <andrew@andrewhwaller.com>
2022-07-13 18:36:38 -0500

    Move NavLink file and update styling

--- src/components/NavLink.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let label: string;
-	export let href: string;
-</script>
-
-<a {href} class="text-gray-600 group flex items-center px-1.5 py-1.5 text-xxs rounded-md">
-	<slot name="icon" />
-	{label}
-</a>
---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export let label: string;
+	export let href: string;
+</script>
+
+<a {href}
+	 class="{$page.url.pathname === href ? 'bg-gray-200' : ''}"
+>
+	<slot name="icon" />
+	{label}
+</a>
+
+<style>
+	a {
+		@apply text-gray-600 flex items-center px-1 py-1 text-xxs rounded-md;
+	}
+</style>
